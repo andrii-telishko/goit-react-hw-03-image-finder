0ff4381b8d002eed0a9548bb265864e793890ba5
Andrii Telishko <telishkoav@gmail.com>
2021-06-18 17:29:47 +0300

    return error

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ const KEY = '21283413-606cd1182a523c739b6934f12';
 const fetchData = ({ searchQuery = '', currentPage = 1, pageSize = 15 }) => {
   return axios
     .get(
-      `https:pixabay.com/api/?q=${searchQuery}&page=${currentPage}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${pageSize}`
+      `https://pixabay.com/api/?q=${searchQuery}&page=${currentPage}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${pageSize}`
     )
     .then(response => response.data.hits);
 };
